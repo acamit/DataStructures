488b7984b73b9a8f73acbd4247bd6c41c6ad4538
acamit84 <acamit84@gmail.com>
2023-08-23 02:24:38 -0400

    Add more array solutions

---
+++ ArrayQuestions/SegregateOddEven.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayQuestions
+{
+    internal class SegregateOddEven
+    {
+        public void Segregate(int[] arr)
+        {
+            int n = arr.Length;
+            int i = -1;
+            int j = 0;
+            while (j != n)
+            {
+                if (arr[j] % 2 == 0)
+                {
+                    ++i;
+                    Swap(arr, i, j);
+                }
+                ++j;
+            }
+
+           
+        }
+        public void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
---
+++ MaximumMinimumForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class MaximumMinimumForm
+{
+    public void Rearrange(int[] arr)
+    {
+        int n = arr.Length;
+        int[] temp = new int[n];
+
+        int small = 0;
+        int large = n - 1;
+
+        bool flag = true;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (flag)
+            {
+                temp[i] = arr[large];
+                large--;
+            }
+            else
+            {
+                temp[i] = arr[small];
+                small++;
+            }
+            flag = !flag;
+            Array.Copy(temp, arr);
+        }
+    }
+
+    public void RearrangeWithoutExtraSpace(int[] arr)
+    {
+        int n = arr.Length;
+        int max_index = n - 1;
+        int min_index = 0;
+        int maxele = arr[max_index] + 1; // add one to the largest element. This will be the base
+
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i % 2 == 0) // even index
+            {
+                arr[i] += (arr[max_index] % maxele) * maxele;
+                max_index--;
+            }
+            else
+            {
+                arr[i] += (arr[min_index] % maxele) * maxele;
+                min_index++;
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = arr[i] / maxele;
+        }
+    }
+
+
+}
