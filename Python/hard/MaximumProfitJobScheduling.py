ada8acb5f68044849ec073eed13e7be378b52206
amit chawla <acamit84@gmail.com>
2024-03-04 18:29:17 -0500

    Add a few hard problems

---
+++ Python/hard/MaximumProfitJobScheduling.py
@@ -0,0 +1,28 @@
+import bisect
+
+
+class Solution:
+    def jobScheduling(self, startTime, endTime, profit):
+        intervals = sorted(zip(startTime, endTime, profit))
+        cache = {}
+        def dfs(i):
+            if i == len(intervals):
+                return 0
+            if i in cache:
+                return cache[i]
+            # don't include
+            res = dfs(i+1)
+            bisect.bisect(intervals, (intervals[i][1], -1, -1)) # build in binary search for python
+            # j = i+1
+            # while j<len(intervals):
+            #     if intervals[i][1] <= intervals[j][0]:
+            #         break
+            #     j+=1
+
+            # include
+            res = max(intervals[i][2]+ dfs(j))
+            cache[i] = res
+
+            return res
+
+        return dfs(0)
