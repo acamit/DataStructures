a37d149e60a3bed24ee1de1d0f8999dbd27ce4ee
amit chawla <acamit84@gmail.com>
2024-03-25 10:07:33 -0400

    Add a comment

--- Python/DynamicProgramming/MaximumContiguousProductSubarray.py
+++ Python/DynamicProgramming/MaximumContiguousProductSubarray.py
@@ -7,7 +7,7 @@ class Solution:
             if n == 0:
                 curMin, curMax = 1, 1
                 continue
-            tmp = curMax * n
+            tmp = curMax * n # needed to prevent override on line 11
             curMax = max(tmp, curMin * n, n)  # [-1, 8] n is required for such cases
             curMin = min(tmp, curMin * n, n)  # [-1, -8]
 
---
+++ Python/backtracking/CombinationSum.py
@@ -0,0 +1,21 @@
+from ast import List
+
+
+class Solution:
+    def combinationSum(self, candidates:List[int], target:int) -> List[List[int]]:
+        res = []
+
+        def dfs(i, cur, total):
+            if total == target:
+                res.append(cur.copy())
+                return
+            if i >= len(candidates) or total > target:
+                return
+            
+            cur.append(candidates[i])
+            dfs(i, cur, total+candidates[i])
+            cur.pop()
+
+            dfs(i+1, cur, total)
+                  
+        return res
